da7edd4084d809c46f107f06cbfb6bdf45d95a2d
Anhuan162 <huan1622k3@gmail.com>
2025-07-03 12:00:38 +0700

    Clean start

--- java-cores/src/oop/abstract_class_sample/PlayBadminton.java
+++
@@ -1,5 +0,0 @@
-package oop.abstract_class_sample;
-
-public interface PlayBadminton {
-    String playBadminton();
-}
--- java-cores/src/oop/abstract_class_sample/PlayFootball.java
+++
@@ -1,5 +0,0 @@
-package oop.abstract_class_sample;
-
-public interface PlayFootball {
-    String playFootball();
-}
